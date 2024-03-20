bdb9c6d4430348af62dab66fc63a352a95b15624
truongthuc14 <140226214+truongthuc14@users.noreply.github.com>
2024-03-20 14:56:14 +0700

    Add files via upload
    Bài 2: Viết chương trình đếm và in ra các số chính phương nhỏ hơn n với n là số nguyên dương nhập từ bàn phím. Yêu cầu viết hàm kiểm tra số chính phương và hàm đếm số chính phương. Biết rằng số chính phương là số khi lấy căn bặc 2 có kết quả là nguyên.
    Ví dụ: với n=50, xuất ra màn hình 7 số chính phương là: 49, 36, 25, 16, 9, 4, 1.

---
+++ Source.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int is_square(int x) {
+    if (x < 0) {
+        return 0;
+    }
+    int sqrt_x = (int)sqrt((double)x);
+    return sqrt_x * sqrt_x == x;
+}
+
+
+int count_squares(int n) {
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (is_square(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    printf("Nhập số nguyên dương n: ");
+    scanf("%d", &n);
+
+    // Đếm số lượng số chính phương
+    int count = count_squares(n);
+
+    // In ra các số chính phương
+    printf("Có %d số chính phương nhỏ hơn %d:\n", count, n);
+    for (int i = 1; i <= n; i++) {
+        if (is_square(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    
+
+
+    return 0;
+}
